837099abfb7886ee71fa85333a7359d220c6016a
Tim Johnston <tedwardj11@gmail.com>
2018-11-27 20:17:06 -0500

    created playlist db model

--- heyDJ/heyDJ/urls.py
+++ heyDJ/heyDJ/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('appHeyDJ/', include('appHeyDJ.urls')),
+    path('home/', include('appHeyDJ.urls')),
     path('admin/', admin.site.urls),
 ]
